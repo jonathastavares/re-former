937145c711ceb4f4fe0ef888e0809ceac9be8180
jonathastavares <jonathastavares@id.uff.br>
2021-04-23 16:27:11 -0300

    Create New and Create methods in users_controller, create form for new users in app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
+            redirect_to new_user_path
+        end
     end
 end
