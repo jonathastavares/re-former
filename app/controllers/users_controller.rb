3b8eb428ddd5b786e0d84f2f4136f73268a023ab
jonathastavares <jonathastavares@id.uff.br>
2021-04-23 18:07:02 -0300

    Create edit views and methods in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,22 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            redirect_to edit_user_path(@user.id)
+        else
+            render :new
+        end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
             redirect_to new_user_path
+        else
+            redirect_to edit_user_path(@user.id)
         end
     end
 
