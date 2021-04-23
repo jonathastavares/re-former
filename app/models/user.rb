02a043019fc2d1031eee0c54acbcfd67a1f42178
jonathastavares <jonathastavares@id.uff.br>
2021-04-23 11:57:26 -0300

    Migrate User database and Create User model for validations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+    validates :password, presence: true, length: { minumum: 8, maximum: 25 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 110 }
+end
