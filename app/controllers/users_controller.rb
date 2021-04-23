0771a042d636083f5307945d45f636b97cbfa402
jonathastavares <jonathastavares@id.uff.br>
2021-04-23 17:00:23 -0300

    Modify new user form to work with rails params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
         if @user.save
             redirect_to new_user_path
         end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
     end
 end
