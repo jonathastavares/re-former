b3522fcacf8c71a4bc5b752758edb9d9d6ce22d0
jonathastavares <jonathastavares@id.uff.br>
2021-04-28 13:38:21 -0300

    Comment form and form_tag and create form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to edit_user_path(@user.id)
+      redirect_to root_path
     else
-      render :new
+      @user = User.find(params[:id])
+      redirect_to edit_user_path(@user.id)
     end
   end
 
